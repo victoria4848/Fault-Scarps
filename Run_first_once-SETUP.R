5c33ca74e180f1cf20e9481198659eddfd0111b3
victoria4848 <45283649+victoria4848@users.noreply.github.com>
2018-11-26 17:15:46 +0200

    Update Run_first_once-SETUP.R

--- Run_first_once-SETUP.R
+++ Run_first_once-SETUP.R
@@ -26,8 +26,9 @@ if (!dir.exists('Fault-Scarps-master/Faults')){
   dir.create('Fault-Scarps-master/DEMs_R')
   dir.create('Fault-Scarps-master/Profiles')  # To store the created profiles
   # Create folders for storing outputs from finding crest and base 
-  dir.create('Fault-Scarps-master/Manual_profiles_offsets')
   dir.create('Fault-Scarps-master/Manual_crba')
+  dir.create('Fault-Scarps-master/Manual_profiles_offsets')
+  dir.create('Fault-Scarps-master/Algorithm_offsets')
   dir.create('Fault-Scarps-master/Offsets')
   dir.create('Fault-Scarps-master/Plots')
   dir.create('Fault-Scarps-master/rms_difs')
